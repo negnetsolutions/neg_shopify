e57ed409ad503fddff14bad0be2971230744dc74
Andrew Johnson <andy@negnet.co>
2020-09-24 14:53:17 -0500

    Added all collection to xml feed.

--- src/Controller/Collections.php
+++ src/Controller/Collections.php
@@ -11,6 +11,7 @@ use Drupal\neg_shopify\Settings;
 use Drupal\Core\Cache\CacheableResponse;
 use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\Core\Render\RenderContext;
+use Drupal\Core\Url;
 
 /**
  * Class Collections.
@@ -21,13 +22,6 @@ class Collections extends ControllerBase {
    * Renders /collections/xml/handle.
    */
   public function renderFeed($handle) {
-    $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['field_handle' => $handle]);
-
-    if (count($term) === 0) {
-      throw new NotFoundHttpException('Could not locate collection!');
-    }
-
-    $term = reset($term);
 
     $params = [
       'sort' => Settings::defaultSortOrder(),
@@ -38,18 +32,31 @@ class Collections extends ControllerBase {
       $params['show'] = $show;
     }
 
-    switch ($term->get('field_type')->value) {
-      case 'SmartCollection':
-        $params['collection_rules'] = json_decode($term->get('field_rules')->value, TRUE);
-        $params['collection_disjunctive'] = (bool) $term->get('field_disjunctive')->value;
-        $tags = ShopifyCollection::cacheTags($term->id());
-        break;
+    if ($handle !== 'all') {
+      $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties(['field_handle' => $handle]);
 
-      default:
-        // CustomCollection.
-        $params['collection_id'] = $term->id();
-        $tags = ShopifyCollection::cacheTags($term->id(), FALSE);
-        break;
+      if (count($term) === 0) {
+        throw new NotFoundHttpException('Could not locate collection!');
+      }
+
+      $term = reset($term);
+
+      switch ($term->get('field_type')->value) {
+        case 'SmartCollection':
+          $params['collection_rules'] = json_decode($term->get('field_rules')->value, TRUE);
+          $params['collection_disjunctive'] = (bool) $term->get('field_disjunctive')->value;
+          $tags = ShopifyCollection::cacheTags($term->id());
+          break;
+
+        default:
+          // CustomCollection.
+          $params['collection_id'] = $term->id();
+          $tags = ShopifyCollection::cacheTags($term->id(), FALSE);
+          break;
+      }
+    }
+    else {
+      $tags = ['shopify_product_list'];
     }
 
     $search = new ShopifyProductSearch($params);
@@ -58,7 +65,7 @@ class Collections extends ControllerBase {
     $build = [
       '#theme' => 'shopify-xml-feed',
       '#products' => ShopifyProduct::loadView($products, 'xml_listing'),
-      '#name' => $term->getName(),
+      '#name' => ($handle === 'all') ? 'All' : $term->getName(),
     ];
 
     $cache = [
@@ -69,8 +76,9 @@ class Collections extends ControllerBase {
     ];
 
     $render = NULL;
-    \Drupal::service('renderer')->executeInRenderContext(new RenderContext(), function () use (&$build, &$render, &$term) {
-      $build['#link'] = $term->toUrl()->setAbsolute()->toString();
+    \Drupal::service('renderer')->executeInRenderContext(new RenderContext(), function () use (&$build, &$render, &$term, &$handle) {
+
+      $build['#link'] = ($handle === 'all') ? Url::fromRoute('neg_shopify.all')->setAbsolute()->toString() : $term->toUrl()->setAbsolute()->toString();
       $render = \Drupal::service('renderer')->render($build);
     });
 
