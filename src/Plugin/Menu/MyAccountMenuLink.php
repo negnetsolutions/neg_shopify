02ca60c19d554ecdb7ed0a6f529be56c6f26fc02
Andrew Johnson <andy@negnet.co>
2021-03-05 08:13:58 -0600

    Added my account menu item

---
+++ src/Plugin/Menu/MyAccountMenuLink.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\neg_shopify\Plugin\Menu;
+
+use Drupal\Core\Menu\MenuLinkDefault;
+use Drupal\Core\Menu\StaticMenuLinkOverridesInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * My Account Link.
+ */
+class MyAccountMenuLink extends MenuLinkDefault {
+
+	/**
+	 * The current user.
+	 *
+	 * @var \Drupal\Core\Session\AccountInterface
+	 */
+	protected $currentUser;
+
+	/**
+	 * Constructs a new LoginLogoutMenuLink.
+	 *
+	 * @param array $configuration
+	 *   A configuration array containing information about the plugin instance.
+	 * @param string $plugin_id
+	 *   The plugin_id for the plugin instance.
+	 * @param mixed $plugin_definition
+	 *   The plugin implementation definition.
+	 * @param \Drupal\Core\Menu\StaticMenuLinkOverridesInterface $static_override
+	 *   The static override storage.
+	 * @param \Drupal\Core\Session\AccountInterface $current_user
+	 *   The current user.
+	 */
+	public function __construct(array $configuration, $plugin_id, $plugin_definition, StaticMenuLinkOverridesInterface $static_override, AccountInterface $current_user) {
+		parent::__construct($configuration, $plugin_id, $plugin_definition, $static_override);
+
+		$this->currentUser = $current_user;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+		return new static(
+			$configuration,
+			$plugin_id,
+			$plugin_definition,
+			$container->get('menu_link.static.overrides'),
+			$container->get('current_user')
+		);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getTitle() {
+		return $this->t('My Account');
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getRouteParameters() {
+		if ($this->currentUser->isAuthenticated()) {
+			return [
+				'user' => $this->currentUser->id(),
+			];
+		}
+		return [];
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getRouteName() {
+		if ($this->currentUser->isAuthenticated()) {
+			return 'entity.user.canonical';
+		}
+		else {
+			return 'user.login';
+		}
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getCacheContexts() {
+		return ['user.roles:authenticated'];
+	}
+}
